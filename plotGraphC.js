4e697a806b1abe6f3ece273e0c61304ae0c071f6
pdrozdzewicz3 <76184633+pdrozdzewicz3@users.noreply.github.com>
2021-07-22 12:23:01 -0400

    updated graph and data so it shows womens instead of mens

--- plotGraphC.js
+++ plotGraphC.js
@@ -1,4 +1,4 @@
-// Part 1
+// Part 1 - Create the Trace
 var year = olympics.map(years =>  years.Year);
 var level = olympics.map(levels =>  levels.level_0);
 var trace1 = {
@@ -7,10 +7,14 @@ var trace1 = {
   type: "scatter"
 };
 
+// Create the data array for the plot
 var data = [trace1];
 
+// Define the plot layout
 var layout = {
-  title: "What year USA won Gold in a 100M Dash - Men"
+  title: "What year USA won Gold in a 100M Dash - Women",
+  xaxis: { title: "Level" },
+  yaxis: { title: "Year" }
 };
 
 Plotly.newPlot("scatter-plot", data, layout);
